d3efc3da9d261f15151078f41217c1aa516cb4d8
DemoreDev <leodemore@gmail.com>
2026-04-10 09:42:43 -0300

    corrige path do java

--- src/path_config.py
+++ src/path_config.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 JSON_PATH = BASE_DIR / "json_configs" / "global_hyperparameter_ranges.json"
 
 # Caminho fixo do Java Classpath que já validamos
-MEKA_CLASSPATH = "/home/leodemore/projetoFapesp/ProjetosIC/lib/*"
+MEKA_CLASSPATH = str(BASE_DIR / "lib" / "*")
 
 def load_hyperparameters_json() -> dict:
     # Carrega o JSON de hyperparams (e tipos) para um dicionário
